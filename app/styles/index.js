c6bed889ccce1396ad300809af8e1df5fb59e8e7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-17 11:47:40 +0100

    Created styles/index.js for css styling. Added a background image

---
+++ app/styles/index.js
@@ -0,0 +1,19 @@
+var styles = {
+  container: {
+    backgroundSize: 'cover',
+    backgroundImage: "url('app/images/sky.jpg')",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+  },
+  header: {
+    fontSize: 45,
+    color: '#fff',
+    fontWeight: 100,
+  }
+};
+
+module.exports = styles;
