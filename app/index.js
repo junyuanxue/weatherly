c0a703d13f3ef26b18cd96b4a36a1551d6879ad7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-17 11:15:51 +0100

    Set up routing, moved hello world component to Main.js

--- app/index.js
+++ app/index.js
@@ -1,15 +1,8 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
-var HelloWorld = React.createClass({
-  render: function () {
-    return (
-      <div>Hello World!</div>
-    )
-  }
-});
+var routes = require('./config/routes');
 
 ReactDom.render(
-  <HelloWorld />,
+  routes,
   document.getElementById('app')
 );
