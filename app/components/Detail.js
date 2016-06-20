424be5985289ca6174c6582c1650b73a5cb59cc5
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-20 12:04:16 +0100

    Added a Detail component

---
+++ app/components/Detail.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var styles = require('../styles');
+
+function Detail (props) {
+  return (
+    <div style={styles.container}  className='jumbotron text-center'>
+      <h1> Detail page </h1>
+    </div>
+  )
+}
